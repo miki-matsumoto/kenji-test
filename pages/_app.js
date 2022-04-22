a402dd94ba1989f07067587382048bb98f238759
miki-matsumoto <mikimatsumoto829@gmail.com>
2022-04-22 14:52:05 +0900

    Add script

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+<script src="https://j.wovn.io/1" data-wovnio="key=NXAS-G" async></script>
+    <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
